17b4828965e7460e6bb00748139884473459af87
meenakshy11 <meenakshy@quantumweaveintelligence.com>
2026-04-13 15:36:04 +0530

    chore: remove temporary patch scripts

--- read.py
+++
@@ -1,5 +0,0 @@
-﻿import pathlib
-
-INCIDENTS = pathlib.Path('/app/app/api/v1/incidents.py')
-src = INCIDENTS.read_text()
-print(repr(src[src.find("async def list_all"):src.find("async def list_all")+300]))
