13465bfe956164b289eca5b6fcd2a230d33fc476
meenakshy11 <meenakshy@quantumweaveintelligence.com>
2026-04-21 12:29:55 +0530

    chore: clean up root directory - remove temp scripts, move seed files to scripts/

--- chk.py
+++
@@ -1,5 +0,0 @@
-﻿import pathlib
-src = pathlib.Path('apps/web/src/pages/Hospitals/HospitalDetailPage.tsx').read_text(encoding='utf-8')
-print('has bloodStock:', 'bloodStock' in src)
-print('has Blood Bank Stock:', 'Blood Bank Stock' in src)
-print('has bloodStockApi:', 'bloodStockApi' in src)
--- end.py
+++
@@ -1,4 +0,0 @@
-﻿import pathlib
-p = pathlib.Path('apps/web/src/pages/Hospitals/HospitalDetailPage.tsx')
-src = p.read_text(encoding='utf-8')
-print(repr(src[-150:]))
